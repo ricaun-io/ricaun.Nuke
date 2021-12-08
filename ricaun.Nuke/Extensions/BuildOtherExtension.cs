1966088dc124d26412a6da5cef0f57c24bda9ec8
Luiz Henrique Cassettari <ricaun@users.noreply.github.com>
2021-12-08 12:00:49 -0300

    - Add BuildOtherExtension - Add AssemblyExtension

---
+++ ricaun.Nuke/Extensions/BuildOtherExtension.cs
@@ -0,0 +1,42 @@
+﻿using Nuke.Common.ProjectModel;
+using System;
+using System.Linq;
+
+namespace ricaun.Nuke.Extensions
+{
+    public static class BuildOtherExtension
+    {
+        #region Other
+        /// <summary>
+        /// Build the Other project
+        /// </summary>
+        /// <param name="Solution"></param>
+        /// <param name="projectName"></param>
+        /// <param name="afterBuild"></param>
+        public static void BuildOtherProject(this Solution Solution, string projectName, Action<Project> afterBuild = null)
+        {
+            if (Solution.GetOtherProject(projectName) is Project project)
+            {
+                foreach (var configuration in project.GetReleases())
+                {
+                    project.Build(configuration);
+                }
+                afterBuild?.Invoke(project);
+            }
+        }
+
+        /// <summary>
+        /// Get Other Project
+        /// </summary>
+        /// <param name="Solution"></param>
+        /// <param name="projectName"></param>
+        /// <returns></returns>
+        public static Project GetOtherProject(this Solution Solution, string projectName)
+        {
+            return Solution.GetProjects("*")
+                .FirstOrDefault(p => p.Name.Equals(projectName, StringComparison.OrdinalIgnoreCase));
+        }
+
+        #endregion
+    }
+}
