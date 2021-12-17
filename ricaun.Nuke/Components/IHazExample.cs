45bb42b421424d425903e53c4d38610988e4756e
Luiz Henrique Cassettari <ricaun@users.noreply.github.com>
2021-12-17 13:42:32 -0300

    ## [0.0.9] / 2021-12-17 - Clear Build - Test Native Example Compile - Rename Example Project - Add ICompileExample - Add IHazExample - Add Native Example Compile

---
+++ ricaun.Nuke/Components/IHazExample.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Nuke.Common;
+using Nuke.Common.IO;
+using Nuke.Common.ProjectModel;
+using Nuke.Common.ValueInjection;
+using ricaun.Nuke.Extensions;
+namespace ricaun.Nuke.Components
+{
+    public interface IHazExample : IHazSolution, INukeBuild
+    {
+        /// <summary>
+        /// Folder Example 
+        /// </summary>
+        [Parameter]
+        string Folder => ValueInjectionUtility.TryGetValue(() => Folder) ?? "Release";
+
+        /// <summary>
+        /// Example Project Name
+        /// </summary>
+        [Parameter]
+        string Name => ValueInjectionUtility.TryGetValue(() => Name) ?? $"{Solution.Name}.Example";
+
+        AbsolutePath ExampleDirectory => GetExampleDirectory(GetExampleProject());
+        public Project GetExampleProject() => Solution.GetOtherProject(Name);
+        public AbsolutePath GetExampleDirectory(Project project) => project.Directory / "bin" / Folder;
+    }
+}
