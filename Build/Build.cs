5e724facdba381e998e87f1eefd2c6fc91bf612b
Luiz Henrique Cassettari <ricaun@users.noreply.github.com>
2022-12-21 15:44:40 -0300

    Ignore If IShowGitVersion fails

--- Build/Build.cs
+++ Build/Build.cs
@@ -7,22 +7,3 @@ class Build : NukeBuild, IPublishPack, ICompileExample, ITest, IShowGitVersion
 {
     public static int Main() => Execute<Build>(x => x.From<IPublishPack>().Build);
 }
-
-public interface IShowGitVersion : IHazGitVersion, IHazChangelog, IClean
-{
-    Target ShowGitVersion => _ => _
-        .TriggeredBy(Clean)
-        .Executes(() =>
-        {
-            // GitVersion.BranchName
-            Serilog.Log.Information(GitVersion.BranchName);
-
-            // GetReleaseNotes
-            Serilog.Log.Information(GetReleaseNotes());
-
-            // Test DownloadFile
-            ricaun.Nuke.Extensions.HttpClientExtension.DownloadFileRetry(
-                @"https://api.github.com/repos/ricaun-io/ricaun.Nuke/releases/latest",
-                System.IO.Path.Combine(BuildAssemblyDirectory, $"latest.json"));
-        });
-}
