9cd908fbc03f0f30f156a993824b6734f15669d0
Luiz Henrique Cassettari <ricaun@gmail.com>
2024-12-17 14:08:06 -0300

    Add version `Information` in the `CommonExtension`.

--- ricaun.Nuke/CommonExtension.cs
+++ ricaun.Nuke/CommonExtension.cs
@@ -1,4 +1,6 @@
 ﻿using Nuke.Common;
+using Nuke.Common.Utilities;
+using System.Linq;
 
 namespace ricaun.Nuke
 {
@@ -13,7 +15,33 @@ namespace ricaun.Nuke
         /// <typeparam name="T"></typeparam>
         /// <param name="nukeBuild"></param>
         /// <returns></returns>
-        public static T From<T>(this T nukeBuild) where T : INukeBuild => (T)(object)nukeBuild;
+        public static T From<T>(this T nukeBuild) where T : INukeBuild
+        {
+            ShowVersion();
+            return (T)(object)nukeBuild;
+        }
+
+        internal static void ShowVersion()
+        {
+            Information();
+            var assemblyName = typeof(CommonExtension).Assembly.GetName().Name;
+            foreach (var item in System.AppDomain.CurrentDomain.GetAssemblies()
+                .Where(e => e.GetName().Name.StartsWith(assemblyName, System.StringComparison.InvariantCultureIgnoreCase)))
+            {
+                Information($"{item.GetName().Name} {item.GetVersionText()}");
+            }
+        }
+
+        internal static void Information(string text = null)
+        {
+            try
+            {
+                // internal static void Information(string text = null)
+                typeof(Host).GetMethod("Information",System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Static)
+                    .Invoke(null, new object[] { text });
+            }
+            catch { };
+        }
     }
 }
 
