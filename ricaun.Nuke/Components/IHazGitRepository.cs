0b5729b6588f844499725cd834bf7da9a26c0338
Luiz Henrique Cassettari <ricaun@gmail.com>
2024-12-18 15:59:40 -0300

    Fix `SkipForked`

--- ricaun.Nuke/Components/IHazGitRepository.cs
+++ ricaun.Nuke/Components/IHazGitRepository.cs
@@ -58,7 +58,10 @@ namespace ricaun.Nuke.Components
             if (EnableForkedRepository)
                 return false;
 
-            return IsGitRepositoryForked();
+            if (IsGitRepositoryForked()) 
+                return true;
+            
+            return false;
         }
         /// <summary>
         /// IsGitRepositoryForked
