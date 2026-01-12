1ed822edf2002fc8c19e135d15a41751df7f3af0
ricaun <ricaun@gmail.com>
2026-01-12 15:15:47 -0300

    1.10.0-alpha.3 - Befor and After Compile

--- Build/Build.cs
+++ Build/Build.cs
@@ -3,7 +3,7 @@ using Nuke.Common.Execution;
 using ricaun.Nuke;
 using ricaun.Nuke.Components;
 
-class Build : NukeBuild, IPublishPack, ICompileExample, ITest, IShowGitVersion, IAzureSignTool, IPrePack, ILocalAssetRelease, ICompileAfter, ICompileBefore
+class Build : NukeBuild, IPublishPack, ICompileExample, ITest, IShowGitVersion, IAzureSignTool, IPrePack, ILocalAssetRelease
 {
     public void ReleaseAsset(ReleaseAssets releaseAssets) { }
     IAssetRelease IHazAssetRelease.AssetRelease => new AssetRelease();
